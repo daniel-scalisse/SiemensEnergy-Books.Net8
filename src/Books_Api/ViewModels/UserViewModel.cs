1cb544cb584e955f4a0b955377b922df4b1e40d4
Daniel Cavalcante Scalisse <daniel_scalisse@yahoo.com.br>
2026-01-03 13:44:19 -0300

    Update UserViewModel.cs
    Correção de mensagens de validação no front-end.

--- src/Books_Api/ViewModels/UserViewModel.cs
+++ src/Books_Api/ViewModels/UserViewModel.cs
@@ -6,21 +6,21 @@ namespace Books_Api.ViewModels
     public class RegisterUserViewModel
     {
         [Required(ErrorMessage = "The {0} field is required")]
-        [EmailAddress(ErrorMessage = "The {0} field is in an invalid format.")]
+        [EmailAddress(ErrorMessage = "The {0} field is in an invalid format")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "The {0} field is required")]
         [StringLength(100, ErrorMessage = "The {0} field must be between {2} and {1} characters", MinimumLength = 6)]
         public string Password { get; set; }
 
-        [Compare("Password", ErrorMessage = "The passwords don't match.")]
+        [Compare("Password", ErrorMessage = "The passwords don't match")]
         public string ConfirmPassword { get; set; }
     }
 
     public class LoginUserViewModel
     {
         [Required(ErrorMessage = "The {0} field is required")]
-        [EmailAddress(ErrorMessage = "The {0} field is in an invalid format.")]
+        [EmailAddress(ErrorMessage = "The {0} field is in an invalid format")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "The {0} field is required")]
